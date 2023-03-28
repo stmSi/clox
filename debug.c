5f754984feccfe1f8fc5dafd197a966e1c1a5713
stmSi <stm1998sithumyo@gmail.com>
2023-03-28 19:45:57 +0630

    17. compiling expressions (copy paste)

--- debug.c
+++ debug.c
@@ -14,7 +14,7 @@ void disassembleChunk(Chunk *chunk, const char *name) {
 int disassembleInstruction(Chunk *chunk, int offset) {
   printf("%04d ", offset);
   if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
-    printf("  asdf | ");
+    printf("   | ");
   } else {
     printf("%4d ", chunk->lines[offset]);
   }
